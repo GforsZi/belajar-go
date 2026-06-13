c270ba4a73636774257e72589b8ff52d59d5ca3a
GforsZi <givaldigumelarsetiawan@gmail.com>
2026-06-13 15:06:00 +0700

    learn more about data type variable

--- hello.go
+++ hello.go
@@ -10,15 +10,30 @@ func main() {
 	greating := "welcome"
 
 	var (
-		a int = 50
-		b int = 30
+		a       uint    = 50 // uint8 uint16 uint32 uint64
+		b       int     = 30 // int8 int16 int32 int64
+		boolean bool    = true
+		float   float32 = 3.14 // float32 float64
 	)
+
 	const c int = 60
 
-	fmt.Println(a, b+c)
+	var array_type = [...]int{3, 6, 8, 5, 2}
+	empty_array := [5]int{}
+	custom_array_potition_value := [5]int{1: 2, 3: 1}
+
+	if boolean {
+		array_type[0] = 1
+		fmt.Println(len(array_type))
+		fmt.Println(array_type)
+		fmt.Println(empty_array)
+		fmt.Println(custom_array_potition_value)
+	}
+
+	fmt.Println(a, b+c, float*float)
 	fmt.Printf("%b %+d %o %#X %04d \n", a, b, a, a, b)
 
 	fmt.Print(text, "\n")
 	fmt.Print(word, " ", greating, "\n")
-	fmt.Printf("my number is %v and %#v \n", a, greating)
+	fmt.Printf("my number is %v and %#v type %T %%\n", a, greating, greating)
 }
