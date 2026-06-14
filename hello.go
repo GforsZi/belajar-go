7969f8ebc6b63b2c53582bba31f0b8f43c8a2c4d
GforsZi <givaldigumelarsetiawan@gmail.com>
2026-06-14 16:31:20 +0700

    learn more about array/slice, operators, and conditions

--- hello.go
+++ hello.go
@@ -11,7 +11,7 @@ func main() {
 
 	var (
 		a       uint    = 50 // uint8 uint16 uint32 uint64
-		b       int     = 30 // int8 int16 int32 int64
+		b       int     = 3  // int8 int16 int32 int64
 		boolean bool    = true
 		float   float32 = 3.14 // float32 float64
 	)
@@ -22,6 +22,32 @@ func main() {
 	empty_array := [5]int{}
 	custom_array_potition_value := [5]int{1: 2, 3: 1}
 
+	slice_text := []string{"here", "we", "go"}
+	arrslice := array_type[3:5]
+	arrslice[1] = 15
+	arrslice = append(arrslice, 1, 2)
+	fmt.Printf("slice = %v %d \n", arrslice, len(arrslice))
+	fmt.Println(len(slice_text))
+	fmt.Println(cap(slice_text))
+
+	make_slice := make([]int, 4, 5) // If the capacity parameter is not defined, it will be equal to length.
+	fmt.Printf("make: %v %d %d \n", make_slice, len(make_slice), cap(make_slice))
+	append_slices := append(arrslice, make_slice...)
+	fmt.Printf("slices = %v \n", append_slices)
+
+	neededArr := array_type[:len(array_type)-2]
+	arrCopy := make([]int, len(neededArr))
+	copy(arrCopy, neededArr)
+	fmt.Printf("copy  %v \n", arrCopy)
+
+	b |= 5
+	fmt.Printf("sum is %v because b before is %03b and 5 is %03b and now b is %03b \n", b, 3, 5, b)
+	b ^= 5
+	fmt.Printf("sum is %v because b before is %b and 5 is %03b and now b is %03b \n", b, 7, 5, b)
+	b &= 4
+	fmt.Printf("sum is %v because b before is %03b and 4 is %03b and now b is %03b \n", b, 2, 4, b)
+
+	println((b > c) || (b < c))
 	if boolean {
 		array_type[0] = 1
 		fmt.Println(len(array_type))
